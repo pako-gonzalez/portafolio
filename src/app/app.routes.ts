013044d2a32e73523a3a856bfd26a1f23a9e5033
Pako Gonzalez <francisko.gonzalez@gmail.com>
2017-09-16 23:36:34 +0200

    Routes added

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import{
+  AboutComponent,
+  PortafolioComponent,
+  ProductComponent
+} from "./components/index.pages";
+
+const app_routes: Routes = [
+  { path: '', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'product', component: ProductComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const app_routing = RouterModule.forRoot(app_routes, { useHash: true});
