a916da6f655d5260d98f26f5a85b79b48cc72cdc
Pako Gonzalez <francisko.gonzalez@gmail.com>
2017-09-18 21:03:06 +0200

    Firebase service ok

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -6,13 +6,29 @@ export class InformationService {
 
   info:any = {};
   loaded:boolean = false;
+  loaded_about:boolean = false;
+  team:any[] = [];
 
   constructor(public http:Http ) {
+    this.load_info();
+    this.load_about();
+  }
+
+  public load_info(){
     this.http.get("assets/data/info.page.json")
       .subscribe( data =>{
-          console.log(data.json());
+          //console.log(data.json());
           this.loaded = true;
           this.info = data.json();
+      });
+  }
+
+  public load_about(){
+    this.http.get("https://webpage-7a336.firebaseio.com/team.json")
+      .subscribe( data =>{
+          console.log(data.json());
+          this.loaded_about = true;
+          this.team = data.json();
       });
   }
 
